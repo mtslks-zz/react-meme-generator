249c3d84b101f316e88f32196f5034aac96f08a6
Mathias Lukas <mathias.lukas@me.com>
2021-09-15 18:31:19 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { css } from '@emotion/react';
+import { useState } from 'react';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/*       <Memeframe />
+      <Memegenerator /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { useState } from 'react';
+
+function Header() {
+  return (
+    <div>
+      <h1>Generate Your Custom Meme</h1>
+      <br />
+      <h2>Express yourself by filling the two blank fields</h2>
+    </div>
+  );
+}
+
+export default Header;
